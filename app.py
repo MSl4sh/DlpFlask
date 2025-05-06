75dac77c47d2162aff85ee6964c8658e6518b516
unknown <slashhable@gmail.com>
2025-05-06 22:19:37 +0200

    add class for hotels, xml for hotels data and fix sub-navigation position

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template, redirect, url_for,request
+from hotels import Hotel
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 if __name__ == "__main__":
     app.run(debug=True)
 
+def load_hotels():
+    tree = ET.parse('data/hotels.xml')
+    root = tree.getroot()
+    hotels = []
+    for hotel_elem in root.findall('hotel'):
+        hotel = Hotel(
+            name=hotel_elem.find('nom').text,
+            rating=int(hotel_elem.find('etoiles').text),
+            description=hotel_elem.find('description').text,
+            location=hotel_elem.find('localisation').text,
+            image=hotel_elem.find('image_url').text
+        )
+        hotels.append(hotel)
+    return hotels
 
 @app.route("/")
 def base():
@@ -15,5 +31,6 @@ def home():
 
 @app.route("/hebergement")
 def hebergement():
-    return render_template("hebergements.html")
+    hotels = load_hotels()
+    return render_template("hebergements.html", hotels=hotels)
 
---
+++ hotels.py
@@ -0,0 +1,7 @@
+class Hotel:
+    def __init__(self, name, rating, description, location, image):
+        self.name=name
+        self.rating=rating
+        self.description=description
+        self.location=location
+        self.image=image
